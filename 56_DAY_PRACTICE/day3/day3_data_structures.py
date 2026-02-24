0516bbe04145dc771686f21e6cae3ee67a20011e
Krishna Vardhan <krishnavardhan07@gmail.com>
2026-02-25 01:10:02 +0530

    day-03: Lists,tuples,dicts,advanced sql

---
+++ 56_DAY_PRACTICE/day3/day3_data_structures.py
@@ -0,0 +1,15 @@
+# LIST
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  # apple
+fruits.append("date")
+print(fruits)
+
+# TUPLE
+coordinates = (10, 20, 30)
+print(coordinates[0])
+
+# DICT
+person = {"name": "Krishna", "city": "Kadapa", "skills": ["Python", "GCP"]}
+print(person["name"])
+person["goal"] = "GenAI Engineer"
+print(person)
