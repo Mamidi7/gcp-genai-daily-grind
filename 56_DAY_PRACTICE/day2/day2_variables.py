fb8473269927bf84a6d72da76a88006c530b60cf
Krishna Vardhan <krishnavardhan07@gmail.com>
2026-02-24 17:30:29 +0530

    Day 2: Python Variables & Data Types

---
+++ 56_DAY_PRACTICE/day2/day2_variables.py
@@ -0,0 +1,13 @@
+name = 'krishna'
+city = 'kadapa'
+years_experience = 3
+is_employed = True
+current_salary = 100000
+
+print(f"Name: {name}, City: {city}, Years Experience: {years_experience}, Is Employed: {is_employed}, Current Salary: {current_salary}")
+
+print(type(name))
+print(type(city))
+print(type(years_experience))
+print(type(is_employed))
+print(type(current_salary))
