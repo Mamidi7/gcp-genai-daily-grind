6d6e023cd2cc5dc00db0e602a58cf8614cb8ed8e
Krishna Vardhan <krishnavardhan07@gmail.com>
2026-02-25 00:19:24 +0530

    Day 3: Loops & Conditionals

---
+++ 56_DAY_PRACTICE/day3/day3_loops.py
@@ -0,0 +1,19 @@
+# Exercise 1: If/Else
+number = 105
+if number > 100:
+    print("Century ra bava!")
+else:
+    print("Keep trying!")
+
+# Exercise 2: For Loop - Even numbers 1-20
+print("\nEven numbers:")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+
+# Exercise 3: While Loop - Countdown 10 to 1
+print("\nCountdown:")
+count = 10
+while count >= 1:
+    print(count)
+    count = count - 1
